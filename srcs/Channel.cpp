4069372512801682eb135304653f140de76f32e1
Pedro <dasilva.pedro187@gmail.com>
2023-02-15 13:29:25 +0100

    Channel getters and checkers

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -22,7 +22,7 @@ void Channel::clearflag(uint8_t flagname) {
   channel_flags_ &= ~(1 << flagname);
 }
 
-uint8_t Channel::checkflag(uint8_t flagname) {
+bool Channel::checkflag(uint8_t flagname) const {
   return channel_flags_ >> flagname & 1;
 }
 
@@ -30,7 +30,58 @@ const std::vector<std::string>& Channel::get_users_(void) const {
   return users_;
 }
 
+const std::vector<std::string>& Channel::get_operators_(void) const {
+  return operators_;
+}
+
+const std::vector<std::string>& Channel::get_banned_users_(void) const {
+  return banned_users_;
+}
+
+const std::vector<std::string>& Channel::get_muted_users_(void) const {
+  return muted_users_;
+}
+
+const std::string& Channel::get_channel_password_(void) const {
+  return channel_password_;
+}
+
+const std::string& Channel::get_channel_topic_(void) const {
+  return channel_topic_;
+}
+
 const int&  Channel::get_user_limit_(void) const {
   return channel_user_limit_;
 }
 
+bool  Channel::is_user(std::string user_name) const {
+  for (std::vector<std::string>::iterator it = users_.begin(); it != users_.end(); ++it) {
+    if (user_name == *it)
+      return true;
+  }
+  return false;
+}
+
+bool  Channel::is_operator(std::string user_name) const {
+  for (std::vector<std::string>::iterator it = operators_.begin(); it != operators_.end(); ++it) {
+    if (user_name == *it)
+      return true;
+  }
+  return false;
+}
+
+bool  Channel::is_banned(std::string user_name) const {
+  for (std::vector<std::string>::iterator it = banned_users_.begin(); it != banned_users_.end(); ++it) {
+    if (user_name == *it)
+      return true;
+  }
+  return false;
+}
+
+bool  Channel::is_muted(std::string user_name) const {
+  for (std::vector<std::string>::iterator it = muted_users_.begin(); it != muted_users_.end(); ++it) {
+    if (user_name == *it)
+      return true;
+  }
+  return false;
+}
--- srcs/Channel.hpp
+++ srcs/Channel.hpp
@@ -11,9 +11,18 @@ class Channel {
 
   void setflag(uint8_t flagname);
   void clearflag(uint8_t flagname);
-  uint8_t checkflag(uint8_t flagname);
+  bool checkflag(uint8_t flagname) const;
   const std::vector<std::string>& get_users_(void) const;
+  const std::vector<std::string>& get_operators_(void) const;
+  const std::vector<std::string>& get_banned_users_(void) const;
+  const std::vector<std::string>& get_muted_users_(void) const;
+  const std::string& get_channel_password_(void) const;
+  const std::string& get_channel_topic_(void) const;
   const int&  get_user_limit_(void) const;
+  bool  is_user(std::string user_name) const;
+  bool  is_operator(std::string user_name) const;
+  bool  is_banned(std::string user_name) const;
+  bool  is_muted(std::string user_name) const;
 
  private:
   // std::vector<std::pair<std::string, uint8_t> >	flags_;
